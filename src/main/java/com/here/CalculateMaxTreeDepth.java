7e490c2b7e787479be04e8c5c62d57a0df818245
Artuqu <percikartur@gmail.com>
2023-03-30 19:41:22 +0200

    Binary Tree Depth

---
+++ src/main/java/com/here/CalculateMaxTreeDepth.java
@@ -0,0 +1,19 @@
+package com.here;
+
+
+import com.here.binaryTree.Node;
+
+public class CalculateMaxTreeDepth extends Node {
+
+
+    public static void main(String[] args) {
+        int[] nodes = new int[]{3, 5, 8, 2, 9, 1};
+        for (int i = 0; i < nodes.length; i++) {
+            CalculateMaxTreeDepth.add(nodes[i]);
+            int maxDepth = CalculateMaxTreeDepth.maxDepth(Node.add(nodes[i]));
+            if (i == nodes.length - 1) {
+                System.out.println("Max depth of Binary Tree is " + maxDepth);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/here/binaryTree/Node.java
@@ -0,0 +1,56 @@
+package com.here.binaryTree;
+
+public class Node {
+
+
+    static Node root;
+    private int value;
+    private Node left;
+    private Node right;
+
+    public Node() {
+    }
+
+    public Node(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    public static Node createNode(int value, Node currentNode) {
+        if (currentNode == null) {
+            return new Node(value);
+        }
+        if (value < currentNode.value) {
+            currentNode.left = createNode(value, currentNode.left);
+        } else if (value > currentNode.value) {
+            currentNode.right = createNode(value, currentNode.right);
+        } else {
+            return currentNode;
+        }
+        return currentNode;
+    }
+
+
+    public static Node add(int value) {
+        root = createNode(value, root);
+        return root;
+    }
+
+
+    public static int maxDepth(Node node) {
+        if (node == null)
+            return -1;
+        else {
+
+            int leftDepth = maxDepth(node.left);
+            int rightDepth = maxDepth(node.right);
+
+            if (leftDepth > rightDepth) {
+                return (leftDepth + 1);
+            } else {
+                return (rightDepth + 1);
+            }
+        }
+    }
+}
